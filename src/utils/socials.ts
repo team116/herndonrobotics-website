8ec9e873eb8af02e6ca0914f553c98dbe9b89874
Abdul G Zia <abdulgzia@gmail.com>
2026-06-25 22:32:41 -0400

    add social links

--- src/utils/socials.ts
+++ src/utils/socials.ts
@@ -1,19 +1,27 @@
 export const social_links = [
   {
     social: "facebook",
-    link: "https://example.com"
+    link: "https://www.facebook.com/Team.116/"
   },
   {
     social: "x",
-    link: "https://example.com"
+    link: "https://x.com/team116"
   },
   {
     social: "github",
-    link: "https://example.com"
+    link: "https://github.com/team116/"
   },
   {
     social: "instagram",
-    link: "https://example.com"
+    link: "https://www.instagram.com/herndonrobotics"
+  },
+  {
+    social: "tba_lamp",
+    link: "https://www.thebluealliance.com/team/116"
+  },
+  {
+    social: "youtube",
+    link: "https://www.youtube.com/@herndonrobotics9787"
   },
 ].map((social)=>({
   social: "social/"+social.social+".svg",
