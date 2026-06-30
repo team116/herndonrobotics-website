d7c1eba7377c46913c9a388018c1c0ac35ceb7ac
Abdul G Zia <abdulgzia@gmail.com>
2026-06-30 00:04:26 -0400

    add collection for portoflio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  markdown: {
+    gfm: true,
+  },
+  integrations: [mdx()],
   site: 'https://www.herndonrobotics.org/',
   base: ''
 });
---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
+
+const portfolio = defineCollection({
+  loader: glob({ pattern: "*.mdx", base: "./src/assets/portfolio" }),
+  schema: ({image}) => z.object({
+    title: z.string(),
+    competition_type: z.string(),
+    year: z.coerce.number(),
+    robot_name: z.string(),
+    img: image(),
+  }),
+});
+
+export const collections = { portfolio };
